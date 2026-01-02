73fbdd71d24070af8a3153a9e7abd9a8bd640dc7
AtesiT <atesit@mail.ru>
2026-01-02 23:40:50 +0300

    MADE: added button

--- TaskListAppWithUseStorageManager/ViewControllers/VC2.swift
+++ TaskListAppWithUseStorageManager/ViewControllers/VC2.swift
@@ -8,9 +8,19 @@ final class SecondViewControllerRepeat: UIViewController {
         return textField
     }()
     
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Tap me", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .systemBlue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(textField)
+        view.addSubview(button)
         setConstraints()
     }
     
@@ -18,7 +28,11 @@ final class SecondViewControllerRepeat: UIViewController {
         NSLayoutConstraint.activate([
             textField.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 50),
             textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            button.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 50),
+            button.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 20),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
 }
