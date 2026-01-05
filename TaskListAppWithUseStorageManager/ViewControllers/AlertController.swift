f1b1ca3b979096f930139f88661cdd24e2775420
AtesiT <atesit@mail.ru>
2026-01-05 17:15:37 +0300

    ADD: button to see an alert and constraints

--- TaskListAppWithUseStorageManager/ViewControllers/AlertController.swift
+++ TaskListAppWithUseStorageManager/ViewControllers/AlertController.swift
@@ -1,6 +1,21 @@
 import UIKit
 
 final class AlertController: UIViewController {
+    
+    private lazy var seeAlertButton: UIButton = {
+        let button = FilledButtonFactory(title: "Alert", color: .green, action: UIAction {
+            [unowned self] _ in testTheAlert()
+        })
+        return button.createButton()
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(seeAlertButton)
+        setConstraints()
+    }
+    
     private func showTheAlert(_ title: String, _ message: String) {
         let theAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default)
@@ -15,3 +30,17 @@ final class AlertController: UIViewController {
     }
 }
 
+extension AlertController {
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            seeAlertButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            seeAlertButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            seeAlertButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+}
+
+
+#Preview {
+    AlertController()
+}
