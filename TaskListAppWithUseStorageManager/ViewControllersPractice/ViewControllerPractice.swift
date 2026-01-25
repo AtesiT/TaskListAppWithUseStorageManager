6b352e0c548c01bf11ede5cd172df50d86a3ddcf
AtesiT <atesit@mail.ru>
2026-01-25 22:50:31 +0300

    MADE: base lines to init a new ViewController

---
+++ TaskListAppWithUseStorageManager/ViewControllersPractice/ViewControllerPractice.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ViewControllerPractice: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+#Preview {
+    ViewControllerPractice()
+}
