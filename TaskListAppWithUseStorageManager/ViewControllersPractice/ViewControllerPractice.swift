8a8a47776fc8ea7d497158bc73838251cfa3e95b
AtesiT <atesit@mail.ru>
2026-01-25 23:27:09 +0300

    MADE: only button with minumim requirements

--- TaskListAppWithUseStorageManager/ViewControllersPractice/ViewControllerPractice.swift
+++ TaskListAppWithUseStorageManager/ViewControllersPractice/ViewControllerPractice.swift
@@ -1,8 +1,35 @@
 import UIKit
 
 final class ViewControllerPractice: UIViewController {
+    private lazy var button: UIButton = {
+        let button = FilledButtonFactory(
+            title: "Tap",
+            color: .green,
+            action: UIAction { [unowned self] _ in
+                buttonAction()
+            })
+        return button.createButton()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(button)
+        setConstrains()
+    }
+    
+    private func buttonAction() {
+        return
+    }
+}
+
+private extension ViewControllerPractice {
+    func setConstrains() {
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
 }
 
