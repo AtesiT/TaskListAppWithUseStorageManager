49516c6c5ab674f409ab4549061cc49c298b9492
AtesiT <atesit@mail.ru>
2026-01-25 23:37:18 +0300

    MADE: added textField with minimum requirements

--- TaskListAppWithUseStorageManager/ViewControllersPractice/ViewControllerPractice.swift
+++ TaskListAppWithUseStorageManager/ViewControllersPractice/ViewControllerPractice.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 final class ViewControllerPractice: UIViewController {
+    
+    //  MARK: - Button
     private lazy var button: UIButton = {
         let button = FilledButtonFactory(
             title: "Tap",
@@ -11,10 +13,23 @@ final class ViewControllerPractice: UIViewController {
         return button.createButton()
     }()
     
+    //  MARK: - TextField
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Add Text"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    
+    //  MARK: - Settings
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(button)
+        view.addSubview(textField)
         setConstrains()
     }
     
@@ -28,7 +43,11 @@ private extension ViewControllerPractice {
         NSLayoutConstraint.activate([
             button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            textField.topAnchor.constraint(equalTo: button.topAnchor, constant: 50),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
         ])
     }
 }
