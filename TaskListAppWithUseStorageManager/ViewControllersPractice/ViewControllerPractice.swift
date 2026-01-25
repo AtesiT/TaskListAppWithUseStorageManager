2ab362cae6f07f284ca3d7c1b462e2f67aba31a0
AtesiT <atesit@mail.ru>
2026-01-25 23:40:27 +0300

    MADE: added label with minimum requirements

--- TaskListAppWithUseStorageManager/ViewControllersPractice/ViewControllerPractice.swift
+++ TaskListAppWithUseStorageManager/ViewControllersPractice/ViewControllerPractice.swift
@@ -22,6 +22,13 @@ final class ViewControllerPractice: UIViewController {
         return textField
     }()
     
+    //  MARK: - Label
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Label"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     //  MARK: - Settings
     
@@ -30,6 +37,7 @@ final class ViewControllerPractice: UIViewController {
         view.backgroundColor = .white
         view.addSubview(button)
         view.addSubview(textField)
+        view.addSubview(label)
         setConstrains()
     }
     
@@ -48,6 +56,11 @@ private extension ViewControllerPractice {
             textField.topAnchor.constraint(equalTo: button.topAnchor, constant: 50),
             textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            label.topAnchor.constraint(equalTo: textField.topAnchor, constant: 50),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
         ])
     }
 }
